9c5764db41d7f05e2be9f3a8fcd10bdb747b19a0
Yagya Parihar <yagya.parihar@gmail.com>
2020-08-09 01:46:02 -0600

    Remove inline build config
    AOSP does not handle inline building like other ROMs. I instead built a LunarKernel zImage and have dropped it in device/google/marlin-kernel.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,13 +4,6 @@ BUILD_BROKEN_DUP_RULES := true
 
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := r353983c
-TARGET_KERNEL_SOURCE := kernel/google/marlin
-TARGET_KERNEL_CONFIG := LunarKernel_defconfig
-TARGET_KERNEL_ARCH := arm64
-BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
-
 # Board
 TARGET_BOARD_AUTO := true
 
